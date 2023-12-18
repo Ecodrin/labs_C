4423fde17656fe570696adff9c3ca45a5979a64e
Ecodrin <matvfo@ya.ru>
2023-12-18 23:50:35 +0300

    update Kursovaya

--- Курсовая/numerical_methods.c
+++ Курсовая/numerical_methods.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
+
+long double machine_epsilon();
+///Функция 20 вариант
+long double func1(long double x);
+long double func1_equivalent(long double x);
+long double func1_equivalent_derivative(long double x);
+long double derivative1_func1(long double x);
+long double derivative2_func1(long double x);
+///функция 21 вариант
+long double func2(long double x);
+long double func2_equivalent(long double x);
+long double func2_equivalent_derivative(long double x);
+long double derivative1_func2(long double x);
+long double derivative2_func2(long double x);
+///Методы
+void method_dichotomii(long double a, long double b, long double(*func)(long double x));
+void method_iterations(long double a, long double b, long double(*func)(long double x),
+                       long double(*func_equivalent)(long double x),
+                       long double(*func_equivalent_derivative)(long double x));
+void method_newton(long double a, long double b, long double(*func)(long double x),
+                   long double(*derivative1_func)(long double x),
+                   long double(*derivative2_func)(long double x));
+
+
+int main(){
+    long double a, b;
+    printf("%s", "20 вариант\n");
+    printf("%s", "Введите границы отрезка через пробел: ");
+    scanf("%Lf %Lf", &a, &b);
+    long double epsilon = machine_epsilon(), root = 1.1183;
+    printf("\n\n%s\n", "function 0.1x^2 - x * lnx = 0");
+    printf("\n\n%s %.20LF\n", "Machine Epsilon: ", epsilon);
+    method_dichotomii(1, 2, func1);
+    method_iterations(1, 2, func1, func1_equivalent, func1_equivalent_derivative);
+    method_newton(1, 2, func1, derivative1_func1, derivative2_func1);
+    printf("%s", "21 вариант\n");
+    printf("%s", "Введите границы отрезка через пробел: ");
+    scanf("%Lf %Lf", &a, &b);
+    printf("\n\n%s\n", "tgx - 1/3(tgx)^3 + 1/5(tgx)^5 - 1/3 = 0");
+    method_dichotomii(a, b, func2);
+    method_iterations(a, b, func2, func2_equivalent, func2_equivalent_derivative);
+    method_newton(a, b, func2, derivative1_func2, derivative2_func2);
+    return 0;
+}
+
+
 
 long double machine_epsilon(){
     double epsilon = 1;
@@ -36,12 +81,12 @@ long double func2(long double x){
 }
 
 long double func2_equivalent(long double x){
-    return tanl(x) - 1.0/3.0 * tanl(x) * tanl(x) * tanl(x) + 1.0/5.0 * tanl(x) * tanl(x) * tanl(x) * tanl(x) * tanl(x) - 1.0/3.0 + x;
+    return -tanl(x) + 1.0/3.0 * tanl(x) * tanl(x) * tanl(x) - 1.0/5.0 * tanl(x) * tanl(x) * tanl(x) * tanl(x) * tanl(x) + 1.0/3.0 + x;
 }
 
 long double func2_equivalent_derivative(long double x){
-    return (cosl(2 * x) * cosl(x) * cosl(x) + sinl(x) * sinl(x) * sinl(x) * sinl(x) + cosl(x) *  cosl(x) *  cosl(x) *  cosl(x) *  cosl(x) *  cosl(x)) /
-            (cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x));
+    return (-1 * cosl(2 * x) * cosl(x) * cosl(x) - sinl(x) * sinl(x) * sinl(x) * sinl(x) + cosl(x) *  cosl(x) *  cosl(x) *  cosl(x) *  cosl(x) *  cosl(x)) /
+           (cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x));
 }
 
 long double derivative1_func2(long double x){
@@ -51,9 +96,10 @@ long double derivative1_func2(long double x){
 
 long double derivative2_func2(long double x){
     return (-2 * cosl(x) * cosl(x) * cosl(x) * sinl(2*x) + 4* cosl(x) * cosl(x) * cosl(2*x)* sinl(x)
-    + 4 * sinl(x) * sinl(x) * sinl(x) + 2 * sinl(x) * sinl(x) * sinl(x) * sinl(x) * sinl(x)) /
-            (cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x));
+            + 4 * sinl(x) * sinl(x) * sinl(x) + 2 * sinl(x) * sinl(x) * sinl(x) * sinl(x) * sinl(x)) /
+           (cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x) * cosl(x));
 }
+
 
 void method_dichotomii(long double a, long double b, long double(*func)(long double x)){
     printf("\n\n%s\n", "---------------- Метод дихотомии ----------------");
@@ -74,7 +120,7 @@ void method_dichotomii(long double a, long double b, long double(*func)(long dou
 void method_iterations(long double a, long double b, long double(*func)(long double x),
                        long double(*func_equivalent)(long double x),
                        long double(*func_equivalent_derivative)(long double x)){
-    long double x_last, x, p;
+    long double x_last, x;
     long double epsilon = machine_epsilon();
     x_last = (a + b) / 2;
     x = func_equivalent(x_last);
@@ -98,7 +144,7 @@ void method_iterations(long double a, long double b, long double(*func)(long dou
 void method_newton(long double a, long double b, long double(*func)(long double x),
                    long double(*derivative1_func)(long double x),
                    long double(*derivative2_func)(long double x)){
-    long double x_last, x, p;
+    long double x_last, x;
     long double epsilon = machine_epsilon();
     printf("\n\n%s\n", "---------------- Метод Ньютона -----------------");
     x_last = (a + b) / 2;
@@ -114,25 +160,4 @@ void method_newton(long double a, long double b, long double(*func)(long double 
         printf("value: %.20Lf\n\n", func(x));
     }
 
-}
-
-int main(){
-    long double a, b;
-    printf("%s", "20 вариант\n");
-    printf("%s", "Введите границы отрезка через пробел: ");
-    scanf("%Lf %Lf", &a, &b);
-    long double epsilon = machine_epsilon(), root = 1.1183;
-    printf("\n\n%s\n", "function 0.1x^2 - x * lnx = 0");
-    printf("\n\n%s %.20LF\n", "Machine Epsilon: ", epsilon);
-    method_dichotomii(1, 2, func1);
-    method_iterations(1, 2, func1, func1_equivalent, func1_equivalent_derivative);
-    method_newton(1, 2, func1, derivative1_func1, derivative2_func1);
-    printf("%s", "21 вариант\n");
-    printf("%s", "Введите границы отрезка через пробел: ");
-    scanf("%Lf %Lf", &a, &b);
-    printf("\n\n%s\n", "tgx - 1/3(tgx)^3 + 1/5(tgx)^5 - 1/3 = 0");
-    method_dichotomii(a, b, func2);
-    method_iterations(a, b, func2, func2_equivalent, func2_equivalent_derivative);
-    method_newton(a, b, func2, derivative1_func2, derivative2_func2);
-    return 0;
 }
