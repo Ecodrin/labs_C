ac1e3cd21893560fb56544b3c3de89617b70dafc
Ecodrin <matvfo@ya.ru>
2024-09-15 11:47:09 +0300

    update lab1 sem3

---
+++ sem3/lab1/ex3/ex3.c
@@ -0,0 +1,128 @@
+#include "ex3.h"
+
+
+
+int GetOpts(int argc, char** argv, kOpts *option, double * numbers) {
+	if (argc != 6 && argc != 4) {
+		return 1;
+	}
+	int count_numbers;
+	if (argv[1][0] == '/' || argv[1][0] == '-') {
+			switch (argv[1][1])
+			{
+				case 'q':
+					*option = OPT_Q;
+					count_numbers = 4;
+					break;
+				case 'm':
+					*option = OPT_M;
+					count_numbers = 2;
+					break;
+				case 't':
+					*option = OPT_T;
+					count_numbers = 4;
+					break;
+				default:
+					return 1;
+			}
+		int k = -1;
+		int fl = 0;
+		for(int i = 0; i < count_numbers; ++i){
+			fl = 0;
+			for(int j = 0; argv[i + 2][j] != '\0'; ++j){
+				if(argv[i + 2][j] == '-')
+					fl = 1;
+				if(argv[i + 2][j] >= '0' && argv[i + 2][j] <= '9'){
+					numbers[i] *= 10;
+					numbers[i] += (argv[i + 2][j] - '0');
+					if(k != -1) k += 1;
+				}
+				else if(argv[i + 2][j] == '.') k = 0;
+			}
+			for(int k_null = 0; k_null < k ;++k_null)
+				numbers[i] /= 10.0;
+			k = -1;
+			if(fl)
+				numbers[i] *= -1;
+			// putchar('\n');
+			// printf("%f\n", numbers[i]);
+		}
+	}
+	return 0;
+}
+
+int compare_values(double a, double b, double eps){
+	if(fabs(b - a) < eps) return 0;
+	if(b - a > eps) return 2;
+	return 1;
+}
+
+void solving_quadratic_equations(double a, double b, double c, double eps){
+	double discriminant, x1, x2;
+    discriminant = b * b - 4 * a * c;
+    if (discriminant > eps) {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        printf("Уравнение %f*x^2 + %f*x + %f == 0 имеет решения: %f %f\n", a, b, c, x1, x2);
+    } else if (fabs(discriminant) < eps) {
+        x1 = -b / (2 * a);
+        printf("Уравнение %f*x^2 + %f*x + %f == 0 имеет решениe: %f\n", a, b, c, x1);
+	}
+	else{
+
+        printf("Уравнение %f*x^2 + %f*x + %f == 0 не имеет решений\n", a, b, c);
+	}
+}
+
+void HandlerOptQ(double * numbers){
+	double eps = numbers[0];
+	double a = numbers[1], b = numbers[2], c = numbers[3];
+	if(compare_values(a, b, eps) != 0 && compare_values(b, c, eps) != 0){
+		solving_quadratic_equations(a, b, c, eps);
+		solving_quadratic_equations(a, c, b, eps);
+		solving_quadratic_equations(b, a, c, eps);
+		solving_quadratic_equations(c, b, a, eps);
+		solving_quadratic_equations(b, c, a, eps);
+		solving_quadratic_equations(c, a, b, eps);
+	}
+	else if(compare_values(a, b, eps) == 0 && compare_values(b, c, eps) != 0){
+		solving_quadratic_equations(a, b, c, eps);
+		solving_quadratic_equations(a, c, b, eps);
+		solving_quadratic_equations(c, b, a, eps);
+	}
+	else if(compare_values(a, b, eps) != 0 && compare_values(b, c, eps) == 0){
+		solving_quadratic_equations(a, b, c, eps);
+		solving_quadratic_equations(b, a, c, eps);
+		solving_quadratic_equations(c, b, a, eps);
+	}
+	else{
+		solving_quadratic_equations(a, b, c, eps);
+	}
+}
+
+
+void HandlerOptM(double * numbers){
+	int a = numbers[0];
+	int b = numbers[1];
+	if(a % b == 0)
+		printf("Число %d кратно числу %d\n", a, b);
+	
+	else
+		printf("Число %d не кратно числу %d\n", a, b);
+}
+
+void HandlerOptT(double * numbers){
+	double eps = numbers[0], maxc = numbers[1], minc = numbers[2];
+	for(int i = 1; i <= 3; ++i){
+		if(compare_values(numbers[i], maxc, eps) == 1)
+			maxc = numbers[i];
+		if(compare_values(numbers[i], minc, eps) == 2)
+			minc = numbers[i];
+	}
+	double b = numbers[1] + numbers[2] + numbers[3] - minc - maxc;
+	if(compare_values(minc + b, maxc, eps) == 1)
+		printf("Треугольник со сторонами %f %f %f  может существовать\n", minc, maxc, b);
+	else
+		printf("Треугольник со сторонами %f %f %f  не может существовать\n", minc, maxc, b);
+
+}
--- sem3/lab1/main.c
+++ sem3/lab1/main.c
@@ -1,5 +1,6 @@
 // #include "ex1/ex1.h"
-#include "ex2/ex2.h"
+// #include "ex2/ex2.h"
+#include "ex3/ex3.h"
 
 #include <stdio.h>
 
@@ -22,13 +23,27 @@ int main (int argc, char** argv) {
 	// }
     // handlers[opt](procceed_number);
 
-    printf("limit: %f\n", calculate_e_limit(0.0001));
-	printf("sequence: %f\n", calculate_e_sequence(0.001));
-    printf("dech: %f\n\n", calculate_e_dichotomy(0.001));
+    // printf("limit: %f\n", calculate_e_limit(0.0001));
+	// printf("sequence: %f\n", calculate_e_sequence(0.001));
+    // printf("dech: %f\n\n", calculate_e_dichotomy(0.001));
 
-    printf("limit: %f\n", calculate_pi_limit(0.001));
-	printf("sequence: %f\n", calculate_pi_sequence(0.001));
-    printf("simple iteration: %f\n", calculate_pi_simple_iteration(0.001));
+    // printf("limit: %f\n", calculate_pi_limit(0.001));
+	// printf("sequence: %f\n", calculate_pi_sequence(0.001));
+    // printf("simple iteration: %f\n", calculate_pi_simple_iteration(0.001));
+
+	kOpts opt = 0;
+	double numbers[4];
+	void (*handlers[3])() = {
+	    HandlerOptQ,
+	    HandlerOptM,
+		HandlerOptT
+	};
+
+	if (GetOpts(argc, argv, &opt, numbers)) {
+		printf("%s", "Incorrect option\n");
+		return 1;
+	}
+    handlers[opt](numbers);
 
 	return 0;
 }
