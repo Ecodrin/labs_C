e8e94bdd1f386d5c150a9d509fad0c4edba03c16
Ecodrin <matvfo@ya.ru>
2024-11-17 22:02:38 +0300

    lab5

---
+++ sem3/FundAlg/lab5/ex1/ex1.h
@@ -0,0 +1,53 @@
+#ifndef LAB5_EX1_H
+#define LAB5_EX1_H
+
+#include <cstring>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+int add(int a, int b);
+int sub(int a, int b);
+
+class binary_int {
+   private:
+	int count_bits;
+	bool bit_representation[32];
+
+	binary_int(const bool *number);
+
+	void to_reserve_code();
+
+   public:
+	binary_int(int number = 0);
+
+	bool *get_bit_representation();
+	int get_count_bits() const;
+
+	binary_int operator-();
+	binary_int operator+(binary_int &b);
+	binary_int operator+=(binary_int &b);
+	binary_int operator-(binary_int &b);
+	binary_int operator-=(binary_int &b);
+	binary_int operator*(binary_int &b);
+	binary_int operator*=(binary_int &b);
+	binary_int operator++();
+	binary_int operator++(int);
+	binary_int operator--();
+	binary_int operator--(int);
+
+	binary_int operator<<(int shift);
+	binary_int operator>>(int shift);
+	binary_int operator<<(binary_int &rhs);
+	binary_int operator>>(binary_int &rhs);
+	binary_int operator>>=(binary_int &rhs);
+	binary_int operator<<=(binary_int &rhs);
+	binary_int operator>>=(int rhs);
+	binary_int operator<<=(int rhs);
+
+	std::pair<binary_int, binary_int> to_parts();
+};
+
+std::ostream &operator<<(std::ostream &out, binary_int &binary);
+
+#endif  // LAB5_EX1_H
